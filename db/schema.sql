2068f255ae0ebda1f067e9adc4446a5de76b7e4f
Shelley M <81432121+shelleymcq@users.noreply.github.com>
2021-07-15 14:08:44 -0400

    basic responses from prompts set to all three tables

--- db/schema.sql
+++ db/schema.sql
@@ -13,20 +13,20 @@ CREATE TABLE role (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) UNIQUE NOT NULL,
   salary DECIMAL UNSIGNED NOT NULL,
-  department_id INT UNSIGNED NOT NULL,
-  INDEX dep_ind (department_id),
+  department_id INT UNSIGNED NOT NULL
+  -- INDEX dep_ind (department_id),
   -- on delete cascade allows you to delete data from child tables automatically when you delete the data from the parent table
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+  -- CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
   id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT UNSIGNED NOT NULL,
-  INDEX role_ind (role_id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  manager_id INT UNSIGNED,
-  INDEX man_ind (manager_id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  role_id INT UNSIGNED NOT NULL
+  -- INDEX role_ind (role_id),
+  -- CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  -- manager_id INT UNSIGNED,
+  -- INDEX man_ind (manager_id),
+  -- CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- employeeTracker.js
+++ employeeTracker.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mysql = require('mysql');
 const dotenv = require('dotenv');
 const inquirer = require('inquirer');
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
 
 const app = express();
 dotenv.config();
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -19,26 +21,125 @@ const init = () => {
         {
             name: 'selectOption',
             type: 'rawlist',
-            message: 'Enter new Department, Role, or Employee',
-            choices: ['Department', 'Role', 'Employee', 'EXIT']
+            message: 'What would you like to do?',
+            choices: ['Add Department', 'Add Role', 'Add Employee', 'EXIT']
     })
     .then((answer) => {
-        if (answer.selectOption === 'Department') {
-            console.log('dept');
-            init();
-        } else if (answer.selectOption === 'Role') {
-            console.log('role');
-            init();
-        } else if (answer.selectOption === 'Employee') {
-            console.log('emp');
-            init();
+        if (answer.selectOption === 'Add Department') {
+            addDepartment();
+        } else if (answer.selectOption === 'Add Role') {
+            addRole();
+        } else if (answer.selectOption === 'Add Employee') {
+            addEmployee();
         } else {
             connection.end();
         }
     })
 }
 
+// Add department
+const addDepartment = () => {
+    inquirer.prompt(
+        {
+            name: 'newDepartment',
+            type: 'input',
+            message: 'What is the name of the department?',
+        })
+        .then((answer) => {
+            // insert new department into the db
+            connection.query(
+                'INSERT INTO department SET ?',
+                {
+                    name: answer.newDepartment
+                },
+                (err) => {
+                    if (err) throw err;
+                    console.log('Your department was created successfuly');
+                    init();
+                }
+            );
+    });
+}
 
+// Add role
+const addRole = () => {
+    inquirer.prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the title of the role?',
+            maxLength: 30
+
+        },
+        {
+            name: 'salary',
+            type: 'number',
+            message: 'What is the salary for the role?'
+        },
+        {
+            name: 'departmentId',
+            type: 'number',
+            message: 'What is the department number?'
+        },
+    ])
+    .then((answer) => {
+        // insert new role into the db
+        connection.query(
+            'INSERT INTO role SET ?',
+            {
+                title: answer.title,
+                salary: answer.salary,
+                department_id: answer.departmentId
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('Your role was created successfuly');
+                init();
+            }
+        );
+    });
+}
+
+
+// Add employee
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            name: 'firstName',
+            type: 'input',
+            message: 'What is the employee\'s first name?',
+            maxLength: 30
+
+        },
+        {
+            name: 'lastName',
+            type: 'input',
+            message: 'What is the employee\'s last name?',
+            maxLength: 30
+        },
+        {
+            name: 'roleId',
+            type: 'number',
+            message: 'What is the employee\'s role number?'
+        },
+    ])
+    .then((answer) => {
+        // insert new role into the db
+        connection.query(
+            'INSERT INTO employee SET ?',
+            {
+                first_name: answer.firstName,
+                last_name: answer.lastName,
+                role_id: answer.roleId
+            },
+            (err) => {
+                if (err) throw err;
+                console.log('Your employee was created successfuly');
+                init();
+            }
+        );
+    });
+}
 
 
 
@@ -46,7 +147,7 @@ const init = () => {
 connection.connect((err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
-    // run function on connection
+    // run app on connection
     init();
   });
 
